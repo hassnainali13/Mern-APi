471ecb194f547adac1baa546c847a134044a6a80
HassnainAli <ahassnainsaleem964@gmail.com>
2024-05-26 04:34:58 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React, { useState } from 'react';
+import ProductForm from './Components/ProductForm';
+import ProductList from './Components/ProductList';
+
+
+
+
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+  const toggleForm = () => {
+    setShowForm((prevShowForm) => !prevShowForm);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='showfoam'>
+      <button onClick={toggleForm}>
+        {showForm ? 'Hide Product Form' : 'Show Product Form'}
+      </button>
+
+      {showForm && <ProductForm />}
+
+      <ProductList />
     </div>
   );
 }
+
+
 
 export default App;
